c3a9884e10fb47609eea2f2b261498fb12956524
Alex Colson <hello@ajcolson.com>
2023-08-09 21:10:20 -0400

    Add old backup restore scripts
    Add old backup/restore scripts I had from 2018 for mapped drives and printers.

---
+++ backup-mapped-drives-printers.ps1
@@ -0,0 +1,31 @@
+﻿###This file was autoinstalled by the Mapped Drive Printer Wizard.
+
+##
+## Written By: Alex Colson <hello@ajcolson.com>
+## 23 Aug 2018
+##
+
+
+
+# Define array to hold identified mapped drives.
+$mappedDrives = @()
+
+# Get a list of the drives on the system, including only FileSystem type drives.
+$drives = Get-PSDrive -PSProvider FileSystem
+
+# Iterate the drive list
+foreach ($drive in $drives) {
+    # If the current drive has a DisplayRoot property, then it's a mapped drive.
+    if ($drive.DisplayRoot) {
+        # Exctract the drive's Name (the letter) and its DisplayRoot (the UNC path), and add then to the array.
+        $mappedDrives += Select-Object Name,DisplayRoot -InputObject $drive
+    }
+}
+
+# Take array of mapped drives and export it to a CSV file.
+$mappedDrives | Export-Csv "$($env:USERPROFILE)\Desktop\mappedDrives.csv"
+
+##Backup printers
+C:\Windows\System32\spool\tools\Printbrm.exe /b /f "$($env:USERPROFILE)\Desktop\printersBackup.printerExport"
+
+exit
---
+++ restore-mapped-drives-printers.ps1
@@ -0,0 +1,20 @@
+﻿###This file was autoinstalled by the Mapped Drive Printer Wizard.
+
+##
+## Written By: Alex Colson <hello@ajcolson.com>
+## 23 Aug 2018
+##
+
+# Import drive list.
+$mappedDrives = Import-Csv "$($env:USERPROFILE)\Desktop\mappedDrives.csv"
+
+# Iterate over the drives in the list.
+foreach ($drive in $mappedDrives) {
+    # Create a new mapped drive for this entry.
+    New-PSDrive -Name $drive.Name -PSProvider "FileSystem" -Root $drive.DisplayRoot -Scope "Global" -Persist -ErrorAction Continue 
+}
+
+##restore printers
+C:\Windows\System32\spool\tools\Printbrm.exe /r /f "$($env:USERPROFILE)\Desktop\printersBackup.printerExport"
+
+exit
